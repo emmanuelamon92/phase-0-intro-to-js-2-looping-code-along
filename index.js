ec004a7d5950a2943d3f87be5a8071d38503fa42
Emmanuel <emmanuelamon92@gmail.com>
2021-04-16 00:20:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"]
+const event = 'birthday'
+
+let writeCards = (names, event) => {
+    const newArr = []
+    for (var i = 0; i < names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+}
+
+let countDown = (num) => {
+    while(num >= 0){
+        console.log(num)
+        num-=1
+    }
+}
